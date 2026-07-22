fcfa9fc8a7bef4307bdae9d56ea79bec1ed35662
iradtaufique <iradukunda.ta@gmail.com>
2026-07-21 22:21:27 -0700

    feature: add Category and task models connected to MYSQL

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Task
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'color', 'created_at']
+    search_fields = ['name']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'status', 'completed', 'category', 'created_at']
+    list_filter = ['status', 'completed', 'category']
+    search_fields = ['title', 'desciption']
+    list_editable = ['status', 'completed']
