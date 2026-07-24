d684cc79f37ea7d6e64935f939886030ef214a1d
iradtaufique <iradukunda.ta@gmail.com>
2026-07-24 09:06:28 -0700

    feat: add REST API with serializers, viewsets, and url Routing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',
+    'django_filters',
     'tasks',
 ]
 
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Category, Task
+from .serializers import CategorySerializer, TaskSerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.select_related('category').all()
+    serializer_class = TaskSerializer
+
+    #Filtering Searching and ordering built in
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    # Custom Action: GET /api/tasks/summary/
+    @action(detail=False, methods=['get'], url_path='summary')
+    def summary(self, request):
+        total = Task.objects.count()
+        pending = Task.objects.filter(status='Pending').count()
+        in_Progress = Task.objects.filter(status='In Process').count()
+        done = Task.objects.filter(status='Done').count()
+
+        return Response({
+            'total': total,
+            'pending': pending,
+            'in_Progress': in_Progress,
+            'done': done
+        }
+        )
+
+
+
